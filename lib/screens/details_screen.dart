8d5625bd0fc9d2a8ba479554ace85aa1b9e041a1
alekseyld <al.lisow1@gmail.com>
2021-02-08 20:53:10 +0300

    - fix Название и описание не полностью видно после создания;

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -15,44 +15,56 @@ class DetailsScreen extends StatelessWidget {
       body: Stack(
         children: <Widget>[
           Scaffold(
-              appBar: AppBar(
-                backgroundColor: Colors.transparent,
-                elevation: 0.0,
-              ),
               extendBodyBehindAppBar: true,
-              body: Stack(
-                children: <Widget>[
-                  PageViewWithIndicator(
-                    photoList: violation.images,
-                  ),
-                  Container(
-                    margin: EdgeInsets.only(top: 380),
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.all(Radius.circular(16.0))
-                    ),
-                    padding: EdgeInsets.all(26.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: <Widget>[
-                        Text(
-                          violation.title,
-                          maxLines: 1,
-                          style: textTheme.headline5,
+              body: ListView(
+                padding: EdgeInsets.zero,
+                children: [
+                  Stack(
+                    children: <Widget>[
+                      PageViewWithIndicator(
+                        photoList: violation.images,
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(top: 380),
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(16.0))),
+                        padding: EdgeInsets.all(26.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: <Widget>[
+                            Text(
+                              violation.title,
+                              maxLines: 1,
+                              style: textTheme.headline5,
+                            ),
+                            const SizedBox(height: 26),
+                            Text(
+                              "Описание:",
+                              style: TextStyle(
+                                  fontFamily: 'RobotoThin',
+                                  fontStyle: FontStyle.italic),
+                              textAlign: TextAlign.start,
+                            ),
+                            const SizedBox(height: 8),
+                            Text(
+                              violation.description,
+                              style: textTheme.bodyText2,
+                            ),
+                          ],
                         ),
-                        const SizedBox(height: 26),
-                        Text(
-                          "Описание:",
-                          style: TextStyle(fontFamily: 'RobotoThin', fontStyle: FontStyle.italic),
-                          textAlign: TextAlign.start,
+                      ),
+                      Positioned(
+                        top: 25,
+                        left: 4,
+                        child: IconButton(
+                          icon: Icon(Icons.arrow_back_outlined),
+                          color: Colors.white,
+                          onPressed: () => Navigator.of(context).pop(),
                         ),
-                        const SizedBox(height: 8),
-                        Text(
-                          violation.description,
-                          style: textTheme.bodyText2,
-                        ),
-                      ],
-                    ),
+                      )
+                    ],
                   ),
                 ],
               )),
